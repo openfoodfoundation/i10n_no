0e5c473e8ed42e3637d0c0bbd836d9fdc9a7f9dc
Rafael Schouten <rafaelschouten@gmail.com>
2015-04-05 20:30:21 +1000

    bugfix seeds

--- seeds.rb
+++ seeds.rb
@@ -11,28 +11,37 @@ unless Spree::Country.find_by_name(ENV['DEFAULT_COUNTRY'])
   Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 end
 
-states = YAML::load_file "db/seeds/default/spree/states.yml"
-states_ids = {}
 country = Spree::Country.find_by_name(ENV['DEFAULT_COUNTRY'])
-suburbs_file = File.join ['db', 'seeds', 'suburbs.csv']
+puts "Country is #{country.to_s}"
+puts "[db:seed] loading states yaml"
+states = YAML::load_file "db/default/spree/states.yml"
+puts "States: #{states.to_s}"
+states_ids = {}
+puts "[db:seed] loading suburbs csv"
+suburbs_file = File.join ['db', 'suburbs.csv']
 
 # -- Seeding States
-unless Spree::State.find_by_name(states[0]['name'])
   puts "[db:seed] Seeding states for " + country.name
 
-  states.each do |state|
-    Spree::State.create!({name: state['name'], abbr: state['abbr'], country: country}, without_protection: true)
-    states_ids[state['abbr']] = Spree::State.where(abbr: s['abbr']).where(country_id: country.id).first.id
+states.each do |id,state|
+  puts id
+  puts "State: " + state.to_s
+  unless Spree::State.find_by_name(state['name'])
+    Spree::State.create!({"name"=>state['name'], "abbr"=>state['abbr'], :country=>country}, without_protection: true)
   end
+  puts "set id"
+  states_ids[state['abbr']] = Spree::State.where(abbr: state['abbr']).where(country_id: country.id).first.id
 end
 
 # -- Seeding suburbs
 
 # Build sql insert statement.
 name = ''
-statement = "INSERT INTO suburbs (postcode,name,state_id,tatitude,longitude) VALUES\n"
+statement = "INSERT INTO suburbs (postcode,name,state_id,latitude,longitude) VALUES\n"
 
-# Format data from suburbs csv
+puts "State ids:" + states_ids.to_s
+
+# Format datatfrom suburbs csv
 CSV.foreach(suburbs_file, {headers: true, header_converters: :symbol}) do |row|
   postcode = row[:postcode]
   name = row[:name]
@@ -42,19 +51,13 @@ CSV.foreach(suburbs_file, {headers: true, header_converters: :symbol}) do |row|
   statement += "(#{postcode},$$#{name}$$,#{state_id},#{lat},#{long}),"
 end
 statement[-1] = ';'
-# puts statement
+
+puts statement
 
 unless Suburb.find_by_name(name)
-  puts "[db:seed] Seeding suburbs for " + OpenFoodNetwork::Config.country_code
+  puts "[db:seed] Seeding suburbs for country.name"
   connection = ActiveRecord::Base.connection()
   connection.execute(statement)
 else
-  puts '[db:seed] Suburbs seeded!'
-end
-
-# -- Landing page images
-unless LandingPageImage.find_by_photo_file_name("potatoes.jpg")
-  LandingPageImage.create photo: File.open(File.join(Rails.root, "lib", "seed_data", "carrots.jpg"))
-  LandingPageImage.create photo: File.open(File.join(Rails.root, "lib", "seed_data", "tomatoes.jpg"))
-  LandingPageImage.create photo: File.open(File.join(Rails.root, "lib", "seed_data", "potatoes.jpg"))
+  puts '[db:seed] Suburbs seeded already!'
 end
